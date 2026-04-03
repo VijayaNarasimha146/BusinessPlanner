b86e1db79cc2cc2bcb55d87b6b15118d43cfce30
VijayaNarasimha146 <101536559+VijayaNarasimha146@users.noreply.github.com>
2026-04-04 01:29:49 +0530

    Add WeekExpander component

---
+++ src/components/WeekExpander.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeekExpander = () => {
+    return (
+        <div>
+            <h2>Week Expander Component</h2>
+            {/* Your week expansion logic goes here */}
+        </div>
+    );
+};
+
+export default WeekExpander;
